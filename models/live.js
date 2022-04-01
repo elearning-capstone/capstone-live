73262a63ad2701041aab258d65673bf340775d9c
kttkkkng <teerawat7971@gmail.com>
2022-04-01 14:26:35 +0700

    init db live.js

---
+++ models/live.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+    const live = sequelize.define("live", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        keygen: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        is_end: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            defaultValue: false,
+        },
+        start_at: {
+            type: DataTypes.DATETIME,
+            allowNull: true,
+        },
+        course_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            }
+        },
+        stream_url: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        video_url: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        }
+    });
+
+    return live;
+}
