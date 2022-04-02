a60442fb58625a1a17d39c9c31cadebb5e62670f
noof3310 <noof3310@gmail.com>
2022-04-02 20:26:03 +0700

    Edit update for lecturer, add update for admin

--- routes/live.js
+++ routes/live.js
@@ -24,10 +24,49 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.put("/info", async (req, res) => {
+    try {
+        const { name, description } = req.body;
+        const live_id = req.query;
+
+        if (typeof live_id != "number") {
+            return res.status(400).json({ message: "invalid live_id" });
+        }
+
+        let count = await live.count({
+            where: {
+                id: live_id,
+            }
+        });
+
+        if (count == 0) {
+            return res.status(404).json({ message: "live not found" });
+        };
+
+        if (name && typeof name != "string" || description && typeof description != "string") {
+            return res.status(400).json({ message: "invalid name or description" });
+        };
+
+        let payload = {};
+        if (name) payload.name = name;
+        if (description) payload.description = description;
+
+        await live.update(payload, {
+            where: {
+                id: live_id,
+            }
+        });
+
+        return res.json({ message: "success" });
+    } catch(err) {
+        return res.status(404).json({ message: "not found" });
+    }
+});
+
 router.put("/", async (req, res) => {
     try {
-        const { name, description, start_at } = req.body;
-        const { live_id, course_id } = req.query;
+        const { name, description, keygen, stream_url, video_url } = req.body;
+        const live_id = req.query;
 
         if (typeof live_id != "number") {
             return res.status(400).json({ message: "invalid live_id" });
@@ -45,14 +84,18 @@ router.put("/", async (req, res) => {
 
         if (name && typeof name != "string" ||
             description && typeof description != "string" ||
-            start_at && typeof start_at != "string") {
-            return res.status(400).json({ message: "invalid name, description or start_at" });
+            keygen && typeof keygen != "string" ||
+            stream_url && typeof stream_url != "string" ||
+            video_url && typeof video_url != "string" ) {
+            return res.status(400).json({ message: "invalid field" });
         };
 
         let payload = {};
         if (name) payload.name = name;
         if (description) payload.description = description;
-        if (start_at) payload.start_at = start_at;
+        if (keygen) payload.keygen = keygen;
+        if (stream_url) payload.stream_url = stream_url;
+        if (video_url) payload.video_url = video_url;
 
         await live.update(payload, {
             where: {
@@ -69,6 +112,38 @@ router.put("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
 
+});
+
+router.delete("/", async (req, res) => {
+    try {
+        const live_id = req.query;
+
+        if (typeof live_id != "number") {
+            return res.status(400).json({ message: "invalid live_id" });
+        }
+
+        let count = await live.count({
+            where: {
+                id: live_id,
+            }
+        });
+
+        if (count == 0) {
+            return res.status(404).json({ message: "live not found" });
+        };
+
+        await live.update({
+            is_end: true
+        } , {
+            where: {
+                id: live_id,
+            }
+        });
+
+        return res.json({ message: "success" });
+    } catch(err) {
+        return res.status(404).json({ message: "not found" });
+    }
 });
 
 module.exports = router;
