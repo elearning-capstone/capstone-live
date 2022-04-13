b59aea20e2617eec74d63486ecf2f2bd9e4e2390
kttkkkng <teerawat7971@gmail.com>
2022-04-13 20:24:17 +0700

    live sync survey get only undone survey

--- routes/live.js
+++ routes/live.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { Op } = require("sequelize");
 const router = express.Router();
+const axios = require("axios");
 const { live, chat, live_survey } = require("../models");
+
+const survey_ip = "http://ip-172-31-37-162.ap-southeast-1.compute.internal:3000";
 
 router.get("/sync", async (req, res) => {
     try {
-        const { live_id, time } = req.query;
+        const { live_id, time, user_id } = req.query;
 
         if (!live_id) {
             return res.status(400).json({ message: "missing live id" });
@@ -38,18 +41,22 @@ router.get("/sync", async (req, res) => {
             where: condition,
         });
 
-        let live_surveys = live_survey.findAll({
+        let live_surveys = await live_survey.findAll({
             attributes: [ "survey_id" ],
             raw: true,
-            where: condition,
+            where: {
+                live_id,
+            },
         });
+
+        live_surveys = await axios.get(survey_ip + "/survey/available", { params: { user_id, survey_id: live_surveys.map(element => element.survey_id) } });
 
         return res.json({
             chat: await chats,
-            live_survey: await live_surveys,
+            live_survey: live_surveys.data,
         });
     } catch(err) {
-        return res.status(404).json({ message: "not found" });
+        return res.status(err.live_surveys.status || 404).json(err.live_surveys.data || { message: "not found" });
     }
 });
 
