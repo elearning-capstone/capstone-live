f4147c8a290559b8f112fe33e87328cf57510811
noof3310 <noof3310@gmail.com>
2022-04-20 14:25:05 +0700

    merge sync

--- routes/live.js
+++ routes/live.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { live } = require("../models");
 const { Op } = require('sequelize')
+const axios = require("axios");
+const { live, chat, live_survey } = require("../models");
+
+const survey_ip = "http://ip-172-31-37-162.ap-southeast-1.compute.internal:3000";
 
 router.post("/", async (req, res) => {
     try {
@@ -185,6 +188,112 @@ router.delete("/", async (req, res) => {
             where: {
                 id: live_id,
             }
+        });
+
+        return res.json({ message: "success" });
+    } catch(err) {
+        return res.status(404).json({ message: "not found" });
+    }
+});
+
+router.get("/sync", async (req, res) => {
+    try {
+        const { live_id, time, user_id } = req.query;
+
+        if (!live_id) {
+            return res.status(400).json({ message: "missing live id" });
+        }
+
+        let count = await live.count({
+            where: {
+                id: live_id,
+            },
+        });
+
+        if (count == 0) {
+            return res.status(404).json({ message: "live not found" });
+        }
+
+        let condition = {
+            live_id,
+        };
+
+        if (time) {
+            condition.updated_at = {
+                [Op.gte]: time,
+            };
+        }
+
+        let chats = chat.findAll({
+            attributes: [ "message", "user_id" ],
+            order: [ "created_at" ],
+            raw: true,
+            where: condition,
+        });
+
+        let live_surveys = await live_survey.findAll({
+            attributes: [ "survey_id" ],
+            raw: true,
+            where: {
+                live_id,
+            },
+        });
+
+        live_surveys = await axios.get(survey_ip + "/survey/available", { params: { user_id, survey_id: live_surveys.map(element => element.survey_id) } });
+
+        return res.json({
+            chat: await chats,
+            live_survey: live_surveys.data,
+        });
+    } catch(err) {
+        return res.status(err.live_surveys.status || 404).json(err.live_surveys.data || { message: "not found" });
+    }
+});
+
+router.post("/chat", async (req, res) => {
+    try {
+        const { live_id, user_id } = req.query;
+        const { message } = req.body;
+
+        let count = await live.count({
+            where: {
+                id: live_id,
+            },
+        });
+
+        if (count == 0) {
+            return res.status(404).json({ message: "live not found" });
+        }
+
+        await chat.create({
+            live_id,
+            user_id,
+            message,
+        });
+
+        return res.json({ message: "success" });
+    } catch(err) {
+        return res.status(404).json({ message: "not found" });
+    }
+});
+
+router.post("/live_survey", async (req, res) => {
+    try {
+        const { live_id, survey_id } = req.query;
+
+        let count = await live.count({
+            where: {
+                id: live_id,
+            },
+        });
+
+        if (count == 0) {
+            return res.status(404).json({ message: "live not found" });
+        }
+
+        await live_survey.create({
+            live_id,
+            survey_id,
         });
 
         return res.json({ message: "success" });
